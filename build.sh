3d8c51bd5bcc33000818d4da957884699692c11b
Hong Minhee <hong@minhee.org>
2024-10-06 15:55:28 +0900

    フォント設定

--- build.sh
+++ build.sh
@@ -10,14 +10,34 @@ else
   exit 1
 fi
 
+mkdir -p "$(dirname "$0")/fonts"
+pushd "$(dirname "$0")/fonts"
+if [[ ! -f NotoSerifCJKjp-VF.ttf ]]; then
+  wget https://github.com/notofonts/noto-cjk/raw/refs/heads/main/Serif/Variable/TTF/NotoSerifCJKjp-VF.ttf
+fi
+if [[ ! -f NotoSansMonoCJKjp-VF.ttf ]]; then
+  wget https://github.com/notofonts/noto-cjk/raw/refs/heads/main/Sans/Variable/TTF/Mono/NotoSansMonoCJKjp-VF.ttf
+fi
+if [[ ! -f NotoSansSymbols2-Regular.ttf ]]; then
+  tmp="$(mktemp -d)"
+  pushd "$tmp"
+  wget https://github.com/notofonts/symbols/releases/download/NotoSansSymbols2-v2.008/NotoSansSymbols2-v2.008.zip
+  7z x NotoSansSymbols2-v2.008.zip
+  popd
+  mv "$tmp/NotoSansSymbols2/full/ttf/NotoSansSymbols2-Regular.ttf" .
+fi
+popd
+
 "$DOCKER" run \
   --interactive \
   --tty \
-  --volume "$(dirname "$0"):/documents/:z" \
+  --volume "$(dirname "$0"):/documents/:Z" \
   docker.io/asciidoctor/docker-asciidoctor \
   asciidoctor-pdf \
   --attribute notitle \
   --attribute text-align=left \
   --attribute source-highlighter=rouge \
+  --attribute pdf-fontsdir="$(dirname "$0")/fonts" \
   --theme ./microblog-theme.yml \
+  --trace \
   README.adoc
